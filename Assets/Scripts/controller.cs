cac15c208492949523e162b8cd24d56d15738002
purplemoonboi <purplemoonone@gmail.com>
2022-01-28 20:32:07 +0000

    Created test scene ready for scripting

--- Assets/Scripts/controller.cs
+++ Assets/Scripts/controller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class firstPerson : MonoBehaviour
+public class controller : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
